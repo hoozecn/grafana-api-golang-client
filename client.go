36173420a8ec29132951bac772826c3d77553ec4
Jeremy Wu <hoozecn@users.noreply.github.com>
2023-10-05 23:11:58 +0800

    fix connection leak (#162)
    * fix connection leak
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Joan López de la Franca Beltran <5459617+joanlopez@users.noreply.github.com>
    Co-authored-by: Leandro López <inkel.ar@gmail.com>

--- client.go
+++ client.go
@@ -92,10 +92,9 @@ func (c *Client) request(method, requestPath string, query url.Values, body io.R
 			continue
 		}
 
-		defer resp.Body.Close()
-
 		// read the body (even on non-successful HTTP status codes), as that's what the unit tests expect
 		bodyContents, err = ioutil.ReadAll(resp.Body)
+		resp.Body.Close() //nolint:errcheck
 
 		// if there was an error reading the body, try again
 		if err != nil {
